df304949fc90f3c2eeee8129878b4c1112f52c7f
mikhailhoh <23017928@myrp.edu.sg>
2024-05-23 20:42:14 +0800

    initial commit

---
+++ Lesson 6/P06_pytest_project/P06_pytest_project/tests/test_calculator.py
@@ -0,0 +1,60 @@
+import pytest
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+    def test_subtract(self):
+        #arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        #act
+        result = cal.subtract(a,b)
+
+        #assert
+        expected = 3087
+        assert result == expected
+    def test_multiply(self):
+        #arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        #act
+        result = cal.multiply(a,b)
+
+        #assert
+        expected = 5332114
+        assert result == expected
+    def test_divide(self):
+        #arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        #act
+        result = a / b
+
+        #assert
+        expected = 3.5016207455429496
+        assert result == expected
+    def test_divide_by_zero(self):
+        cal = Calculator()
+        a, b = 4321, 0
+
+        try:
+            cal.divide(a, b)
+        except ZeroDivisionError as e:
+            assert str(e) == "Division by zero error"
